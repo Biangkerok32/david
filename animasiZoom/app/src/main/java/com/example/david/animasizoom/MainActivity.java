ca589d027bdfc0ab6043d91ac4db85ac6e3fe5b9
DavidSenjayaGit <david@davids-MacBook-Pro.local>
2015-05-13 08:53:22 +0700

    Hasil coba coba animasi sederhana

---
+++ animasiZoom/app/src/main/java/com/example/david/animasizoom/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.david.animasizoom;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    public void onImageClick(View thumbnailView)
+    {
+        ImageView thumbnail = (ImageView)thumbnailView;
+        ImageView animatedImage = (ImageView) findViewById(R.id.animatedImage);
+        animatedImage.setImageDrawable(thumbnail.getDrawable());
+        animatedImage.setVisibility(View.VISIBLE);
+
+        Animation animation
+                = AnimationUtils.loadAnimation(this, R.anim.scale_anim);
+        animatedImage.startAnimation(animation);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
